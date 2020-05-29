5f501764225e9a04c4ea7ef86a53d1a06ee6fe99
tiquiero <tiquiero@163.com>
2020-05-29 15:44:35 +0800

    dataset

--- src/pages/dataSet/index.jsx
+++ src/pages/dataSet/index.jsx
@@ -1,20 +1,17 @@
 import { message, Table, Modal, Form, Input, Button } from 'antd';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { getProject, deleteProject, submit, edit } from './service';
 import { PAGEPARAMS } from '../../const';
-import ModalForm from './components/ModalForm';
 const { confirm } = Modal;
 
 const DataSetList = () => {
   const emptyValue = { Name: '', Info: '' };
   const [project, setProject] = useState({ data: [], total: 0 });
   const [modalFlag, setModalFlag] = useState(false);
-  const [modalType, setModalType] = useState('');
   const [pageParams, setPageParams] = useState(PAGEPARAMS);
   const [editProjectId, setEditProjectId] = useState('');
   const [form] = Form.useForm();
-  const modalFormRef = useRef();
 
   useEffect(() => {
     getData();
@@ -31,28 +28,11 @@ const DataSetList = () => {
     }
   };
 
-  const handleRemove = (id) => {
-    confirm({
-      content: `确定要删除该数据集吗？`,
-      okText: '确定',
-      cancelText: '取消',
-      onOk: async () => {
-        const res = await deleteProject(id);
-        const { successful, msg } = res;
-        if (successful === 'true') {
-          getData();
-        }
-      },
-      onCancel() {},
-    });
-  };
-
   const pageParamsChange = (page, size) => {
     setPageParams({ page: page, size: size });
   };
 
   const onSubmit = () => {
-    console.log('tttttt', modalFormRef.current.hello());
     // form.validateFields()
     //   .then(async (values) => {
     //     if (modalType === 'new') {
@@ -72,37 +52,33 @@ const DataSetList = () => {
 
   const columns = [
     {
-      title: '数据集ID',
-      dataIndex: 'ProjectId',
-      width: 300,
-    },
-    {
-      title: '数据集名称',
+      title: 'Dataset Name',
       dataIndex: 'Name',
     },
     {
-      title: '数据分类',
-      dataIndex: 'type',
-      render: () => <span>图片</span>,
-    },
-    {
-      title: '数据集简介',
+      title: 'Description',
       dataIndex: 'Info',
       ellipsis: true,
       width: 350,
     },
     {
-      title: '操作',
+      title: 'Creator',
+      dataIndex: 'Creator',
+    },
+    {
+      title: 'Update Time',
+      dataIndex: 'Update Time',
+    },
+    {
+      title: 'Update Version',
+      dataIndex: 'Update Version',
+    },
+    {
+      title: 'Operation',
       render: (item) => {
         return (
           <div>
             <a onClick={() => onEditClick(item)}>编辑</a>
-            <a
-              style={{ color: 'red', marginLeft: 10 }}
-              onClick={() => handleRemove(item.ProjectId)}
-            >
-              删除
-            </a>
           </div>
         );
       },
@@ -112,7 +88,6 @@ const DataSetList = () => {
   const onEditClick = (item) => {
     form.setFieldsValue(item);
     setEditProjectId(item.ProjectId);
-    setModalType('edit');
     setModalFlag(true);
   };
 
@@ -123,23 +98,10 @@ const DataSetList = () => {
 
   return (
     <>
-      <Button
-        type="primary"
-        style={{ marginBottom: 14 }}
-        onClick={() => {
-          setModalType('new');
-          resetModal(true);
-        }}
-      >
-        新增数据集
-      </Button>
       <Table
         columns={columns}
         dataSource={project.data}
         rowKey={(r, i) => `${i}`}
-        // rowSelection={{
-        //   type: 'checkbox'
-        // }}
         pagination={{
           total: project.total,
           showQuickJumper: true,
@@ -151,17 +113,31 @@ const DataSetList = () => {
       />
       {modalFlag && (
         <Modal
-          title={`${modalType === 'edit' ? '编辑' : '新增'}数据集`}
+          title='编辑数据集'
           visible={modalFlag}
           onOk={onSubmit}
           onCancel={() => resetModal(false)}
-          okText="提交"
-          cancelText="取消"
+          okText="Submit"
+          cancelText="Cancel"
           destroyOnClose
           maskClosable={false}
-          width={600}
         >
-          <ModalForm ref={modalFormRef}></ModalForm>
+          <Form form={form}>
+            <Form.Item
+              label="Dataset Name"
+              name="Name"
+              rules={[{ required: true }]}
+            >
+              <Input disabled />
+            </Form.Item>
+            <Form.Item
+              label="Description"
+              name="Info"
+              rules={[{ required: true, message: 'Description is required！' }]}
+            >
+              <Input.TextArea placeholder="please enter description" autoSize={{ minRows: 4 }} />
+            </Form.Item>
+          </Form>
         </Modal>
       )}
     </>
