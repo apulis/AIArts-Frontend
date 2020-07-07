a38fbd39aa7482f89dade353612978d9b1e665d4
Han <xjhan1995@gmail.com>
2020-07-07 16:34:15 +0800

    增加 inferenceService 相关接口

---
+++ src/services/inferenceService.js
@@ -0,0 +1,24 @@
+import request from '../utils/request'
+
+
+export async function fetchInferenceList() {
+  return await request('/inferences')
+}
+
+export async function fetchInferenceDetail(id) {
+  return await request('/inferences/' + id)
+}
+
+export async function createInference(data) {
+  return await request('/inferences', {
+    method: 'POST',
+    data,
+  })
+}
+
+export async function startRecognition(id) {
+  return await request(`/inferences/${id}/recognition`, {
+    method: 'POST',
+    data,
+  })
+}
