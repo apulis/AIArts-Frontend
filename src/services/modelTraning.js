b959cbd1cc0e4ab9db2ba25079e21d92119feaaa
Han <xjhan1995@gmail.com>
2020-07-07 17:12:15 +0800

    增加模型训练部分接口

--- src/services/modelTraning.js
+++ src/services/modelTraning.js
@@ -7,3 +7,23 @@ export async function submitModelTraining(data) {
     data,
   })
 }
+
+export async function fetchTrainingDetail(id) {
+  return await request(`/trainings/${id}`)
+}
+
+export async function fetchTrainingLog(id) {
+  return await request(`/trainings/${id}/log`)
+}
+
+export async function stopTraining(id) {
+  return await request(`/api/trainings/${id}/stop`, {
+    method: 'POST'
+  })
+}
+
+export async function removeTraining(id) {
+  return await request(`/api/trainings/${id}`, {
+    method: 'DELETE'
+  })
+}
