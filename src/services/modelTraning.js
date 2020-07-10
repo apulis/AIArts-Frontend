8ff260fcad838572eb18b14798ecb7ed78eaa8a1
Han <xjhan1995@gmail.com>
2020-07-10 17:13:06 +0800

    增加删除训练接口

--- src/services/modelTraning.js
+++ src/services/modelTraning.js
@@ -37,7 +37,7 @@ export async function stopTraining(id) {
   })
 }
 
-export async function removeTraining(id) {
+export async function removeTrainings(id) {
   return await request(`/api/trainings/${id}`, {
     method: 'DELETE'
   })
