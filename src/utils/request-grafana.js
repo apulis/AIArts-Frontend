893ff00b697aa11b49ae3010344224852f2a5c9c
Han <xjhan1995@gmail.com>
2020-07-14 17:06:01 +0800

    删除一个提示

--- src/utils/request-grafana.js
+++ src/utils/request-grafana.js
@@ -98,7 +98,6 @@ request.interceptors.request.use(async (url, options) => {
 });
 
 request.interceptors.response.use((response, options) => {
-  if (options.method === 'DELETE' && response.status === 200) message.success('删除成功！');
   return response;
 });
 
