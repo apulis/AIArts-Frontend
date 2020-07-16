349b41092429c5e634c7d137e0e4cf25b17727b5
Han <xjhan1995@gmail.com>
2020-07-16 16:17:48 +0800

    test push

--- server.js
+++ server.js
@@ -10,20 +10,18 @@ const history = require('connect-history-api-fallback')
 const app = express()
 app.use(bodyParser.json())
 
-app.post('//update', (req, res) => {
-  if (req.body.ref === 'refs/heads/master') {
-    exec('git pull https://cafbe4d081c2ffc3015dc82f78b1c750d245d7fd@github.com/apulis/AIArts.git', (err, result) => {
-      console.log('pull start')
-      if (!err) {
-        console.log('pull over')
-        exec('yarn build', (err, result) => {
-          if (!err) {
-            console.log('build')
-          }
-        })
-      }
-    })
-  }
+app.post('/update', (req, res) => {
+  exec('git pull https://cafbe4d081c2ffc3015dc82f78b1c750d245d7fd@github.com/apulis/AIArts.git', (err, result) => {
+    console.log('pull start')
+    if (!err) {
+      console.log('pull over')
+      exec('yarn build', (err, result) => {
+        if (!err) {
+          console.log('build')
+        }
+      })
+    }
+  })
 })
 app.use(history())
 
