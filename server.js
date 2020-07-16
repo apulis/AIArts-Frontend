44ff235ebe408c74912f20c721d29de455bbc148
Han <xjhan1995@gmail.com>
2020-07-16 15:51:04 +0800

    Update webhooks

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-const http = require('http');
-const path = require('path');
-const { exec } = require('child_process');
+const http = require('http')
+const path = require('path')
+const { exec } = require('child_process')
 
-const bodyParser = require('body-parser');
-const express = require('express');
-const ecstatic = require('ecstatic');
-const history = require('connect-history-api-fallback');
+const bodyParser = require('body-parser')
+const express = require('express')
+const ecstatic = require('ecstatic')
+const history = require('connect-history-api-fallback')
 
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/update', (req, res) => {
-  console.log('body', req.body)
-  // exec('git pull ')
+app.post('//update', (req, res) => {
+  if (req.body.ref === 'refs/heads/master') {
+    exec('git pull https://cafbe4d081c2ffc3015dc82f78b1c750d245d7fd@github.com/apulis/AIArts.git', (err, result) => {
+      if (!err) {
+        exec('yarn build')
+      }
+    })
+  }
 })
 app.use(history())
 
