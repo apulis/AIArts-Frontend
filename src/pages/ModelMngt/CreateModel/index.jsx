b13145a0d25ada6477e510a72042ea1d6a628daa
kgloveyou <hanjiangfeng2007@sina.com>
2020-07-23 10:43:14 +0800

    styles: 调整布局

--- src/pages/ModelMngt/CreateModel/index.jsx
+++ src/pages/ModelMngt/CreateModel/index.jsx
@@ -127,110 +127,116 @@ const CreateModel = props => {
 
   return (
     <>
-    <PageHeader
-      ghost={false}
-      onBack={() => history.push('/ModelManagement/MyModels')}
-      title="创建模型"
-    >
-      <div
-        style={{
-          padding: '24px'
-        }}
+      <PageHeader
+        ghost={false}
+        onBack={() => history.push('/ModelManagement/MyModels')}
+        title="创建模型"
       >
-        <Form
-          form={form}
-          onFinish={onFinish}
-          autoComplete="off"
-          initialValues={{ sourceType: sourceType }}
+        <div
+          style={{
+            padding: '24px'
+          }}
         >
-          <Form.Item
-            {...layout}
-            name="name"
-            label="名称"
-            rules={[{ required: true, message: '名称不能为空!' }]}
-          >
-            <Input placeholder="请输入模型名称" />
-          </Form.Item>
-          <Form.Item
-            labelCol={{ span: 3 }} 
-            wrapperCol={{ span: 14 }}
-            name="description"
-            label="描述"
-            rules={[{ max: 256 }]}
-          >
-            <TextArea rows={4} placeholder="请输入描述信息" />
-          </Form.Item>
-          <Form.Item label="模型文件" rules={[{ required: true }]} name="sourceType">
-            <Radio.Group onChange={e => setSourceType(e.target.value)}>
-              <Radio value={1}>输入模型路径</Radio>
-              <Radio value={2}>上传模型文件</Radio>
-            </Radio.Group>
-          </Form.Item>
-          {sourceType == 1 &&<Form.Item
-            {...layout}
-            name="path"
-            label="存储路径"
-            rules={[{ required: true, message: '存储路径不能为空!' }]}
-          >
-            <Input addonBefore={codePathPrefix} placeholder="请输入存储路径" />
-          </Form.Item>}       
-          {sourceType == 2 && <Form.Item
-            labelCol={{ span: 3 }}
-            wrapperCol={{ span: 14 }}
-            label="上传文件"
-            name="file"
-            rules={[{ required: true, message: '请上传文件！' }]}
-            valuePropName="file"
-          >
-            <Dragger {...uploadProps}>
-              <p className="ant-upload-drag-icon">
-                <InboxOutlined />
-              </p>
-              <p className="ant-upload-text">请点击或拖入文件上传</p>
-              <p className="ant-upload-hint">（只支持上传格式为 .zip, .tar 和 .tar.gz 的文件，且最大不能超过2GB）</p>
-            </Dragger>
-          </Form.Item>}
-          <Form.Item
-            {...layout}
-            label="选择训练作业"
-            required
+          <Form
+            form={form}
+            onFinish={onFinish}
+            autoComplete="off"
+            initialValues={{ sourceType: sourceType }}
           >
             <Form.Item
-              name="job"
-              rules={[{ required: true, message: '训练作业不能为空!' }]}
-              style={{ display: 'inline-block', width: 'calc(95% - 4px)' }}              
+              {...layout}
+              name="name"
+              label="名称"
+              rules={[{ required: true, message: '名称不能为空!' }]}
             >
-              <Input placeholder="请选择训练作业名称" />
+              <Input placeholder="请输入模型名称" />
             </Form.Item>
             <Form.Item
-              style={{ display: 'inline-block', width: 'calc(5% - 4px)', margin: '0 0 0 8px' }}
+              labelCol={{ span: 3 }} 
+              wrapperCol={{ span: 14 }}
+              name="description"
+              label="描述"
+              rules={[{ max: 256 }]}
             >
-              <Button icon={<FolderOpenOutlined />} onClick={showJobModal}></Button>
+              <TextArea rows={4} placeholder="请输入描述信息" />
             </Form.Item>
-          </Form.Item>
-          <Form.Item
-              name="jobId"
-              hidden            
+            <Form.Item
+              {...layout}
+              label="模型文件" 
+              name="sourceType"
+              rules={[{ required: true }]} 
             >
-              <Input type="hidden"/>
-            </Form.Item>          
-          <Form.Item
-            style={{ float: 'right' }}
-          >
-            <Button type="primary" htmlType="submit" disabled={btnDisabled}>立即创建</Button>
-          </Form.Item>
-        </Form>
-      </div>
-    </PageHeader>
-    {/* 选择训练作业弹框 */}
-    {
-      visible && <ModalForm
-      // current={current}
-      visible={visible}
-      onCancel={handleCancel}
-      onSubmit={handleSubmit}
-    />
-    }
+              <Radio.Group onChange={e => setSourceType(e.target.value)}>
+                <Radio value={1}>输入模型路径</Radio>
+                <Radio value={2}>上传模型文件</Radio>
+              </Radio.Group>
+            </Form.Item>
+            {sourceType == 1 &&<Form.Item
+              {...layout}
+              name="path"
+              label="存储路径"
+              rules={[{ required: true, message: '存储路径不能为空!' }]}
+            >
+              <Input addonBefore={codePathPrefix} placeholder="请输入存储路径" />
+            </Form.Item>}       
+            {sourceType == 2 && <Form.Item
+              labelCol={{ span: 3 }}
+              wrapperCol={{ span: 14 }}
+              label="上传文件"
+              name="file"
+              rules={[{ required: true, message: '请上传文件！' }]}
+              valuePropName="file"
+            >
+              <Dragger {...uploadProps}>
+                <p className="ant-upload-drag-icon">
+                  <InboxOutlined />
+                </p>
+                <p className="ant-upload-text">请点击或拖入文件上传</p>
+                <p className="ant-upload-hint">（只支持上传格式为 .zip, .tar 和 .tar.gz 的文件，且最大不能超过2GB）</p>
+              </Dragger>
+            </Form.Item>}
+            <Form.Item
+              {...layout}
+              wrapperCol={{ span: 9 }}
+              label="选择训练作业"
+              required
+            >
+              <Form.Item
+                name="job"
+                rules={[{ required: true, message: '训练作业不能为空!' }]}
+                style={{ display: 'inline-block', width: 'calc(90% - 4px)' }}              
+              >
+                <Input placeholder="请选择训练作业名称" />
+              </Form.Item>
+              <Form.Item
+                style={{ display: 'inline-block', width: 'calc(10% - 4px)', margin: '0 0 0 8px' }}
+              >
+                <Button icon={<FolderOpenOutlined />} onClick={showJobModal}></Button>
+              </Form.Item>
+            </Form.Item>
+            <Form.Item
+                name="jobId"
+                hidden            
+              >
+                <Input type="hidden"/>
+              </Form.Item>          
+            <Form.Item
+              style={{ float: 'right' }}
+            >
+              <Button type="primary" htmlType="submit" disabled={btnDisabled}>立即创建</Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </PageHeader>
+      {/* 选择训练作业弹框 */}
+      {
+        visible && <ModalForm
+        // current={current}
+        visible={visible}
+        onCancel={handleCancel}
+        onSubmit={handleSubmit}
+      />
+      }
     </>  
   );
 };
