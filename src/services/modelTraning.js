32e141e97d4ec9cf34f88b4306044ef71fcecc26
Ti-tanium <1158976264@qq.com>
2020-07-24 16:07:10 +0800

    add: 训练参数接口

--- src/services/modelTraning.js
+++ src/services/modelTraning.js
@@ -1,15 +1,15 @@
 import request from '../utils/request';
-
+import { PAGEPARAMS } from '@/utils/const';
 
 export async function submitModelTraining(data) {
   return await request('/trainings', {
     method: 'POST',
     data
-  })
+  });
 }
 
 export async function fetchAvilableResource() {
-  return await request('/common/resource')
+  return await request('/common/resource');
 }
 
 export async function fetchTrainingList() {
@@ -18,25 +18,39 @@ export async function fetchTrainingList() {
       pageNum: 1,
       pageSize: 999,
     }
-  })
+  });
 }
 
 export async function fetchTrainingDetail(id) {
-  return await request(`/trainings/${id}`)
+  return await request(`/trainings/${id}`);
 }
 
 export async function fetchTrainingLog(id) {
-  return await request(`/trainings/${id}/log`)
+  return await request(`/trainings/${id}/log`);
 }
 
 export async function stopTraining(id) {
   return await request(`/trainings/${id}/stop`, {
     method: 'POST'
-  })
+  });
 }
 
 export async function removeTrainings(id) {
   return await request(`/trainings/${id}`, {
     method: 'DELETE'
-  })
+  });
+}
+
+export async function fetchTemplates(params) {
+  return await request(`/Templates`, { params });
+}
+
+export async function fetchTemplateById(id) {
+  return await request(`Templates/${id}`);
+}
+
+export async function removeTemplate(id) {
+  return await request(`/Templates/${id}`, {
+    method: 'DELETE'
+  });
 }
