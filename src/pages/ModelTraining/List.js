86b729d3a1507b21d8e12fd768d2429ae6e43da1
Han <xjhan1995@gmail.com>
2020-07-27 16:31:09 +0800

    添加名称排序

--- src/pages/ModelTraining/List.js
+++ src/pages/ModelTraining/List.js
@@ -104,6 +104,9 @@ const List = () => {
         return (
           <Link to={`/model-training/${item.id}/detail`}>{item.name}</Link>
         )
+      },
+      sorter(a, b) {
+        return a.name > b.name;
       }
     },
     {
