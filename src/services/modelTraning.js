b1c79b271c00dc6fff8a9e3f22974eff75bf8ccd
Han <xjhan1995@gmail.com>
2020-07-29 14:09:39 +0800

    change method to post

--- src/services/modelTraning.js
+++ src/services/modelTraning.js
@@ -61,7 +61,7 @@ export async function removeTemplate(id) {
 
 export async function saveTrainingParams(data) {
   return await request('/templates', {
-    method: 'PUT',
+    method: 'POST',
     data
   })
 }
