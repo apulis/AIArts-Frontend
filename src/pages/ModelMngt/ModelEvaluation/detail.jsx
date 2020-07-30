a1c0ed0893783824e16e913432a31e86cd77fd40
kgloveyou <hanjiangfeng2007@sina.com>
2020-07-30 18:25:42 +0800

    对接评估详情接口

--- src/pages/ModelMngt/ModelEvaluation/detail.jsx
+++ src/pages/ModelMngt/ModelEvaluation/detail.jsx
@@ -5,6 +5,8 @@ import React, { useState, useEffect, useRef } from 'react';
 import { useParams } from 'umi';
 import moment from 'moment';
 import { fetchEvaluationLog, fetchEvaluationDetail } from './services';
+import { getJobStatus } from '@/utils/utils';
+
 import styles from './index.less';
 
 const EvaluationDetail = props => {
@@ -12,7 +14,7 @@ const EvaluationDetail = props => {
   const modelId = params.id;
 
   const logEl = useRef(null);
-  const [evaluationDetail, setEvaluationDetail] = useState(null);
+  const [evaluationJob, setEvaluationJob] = useState(null);
   const [logs, setLogs] = useState('');
 
   const getEvaluationLog = async () => {
@@ -30,7 +32,7 @@ const EvaluationDetail = props => {
     const res = await fetchEvaluationDetail(modelId);
     const {code, msg, data} = res;
     if (code === 0) {
-      setEvaluationDetail(data);
+      setEvaluationJob(data.job);
     }
   }
 
@@ -42,16 +44,6 @@ const EvaluationDetail = props => {
       message.success('成功获取结果');
     }
   }  
-
-  // const evaluationDetail = {
-  //   modelName: 'hanjf-test2',
-  //   status: 'running',
-  //   engineType: 'apulistech/pytorch:2.0',
-  //   dataset: 'coco',
-  //   createAt: new Date().valueOf(),
-  //   deviceType: 'nvidia_gpu_amd64',
-  //   deviceNum: '2',
-  // };
 
   useEffect(() => {
     // getMockEvaluationDetail();
@@ -65,15 +57,15 @@ const EvaluationDetail = props => {
       title="评估详情"
     >
       <Descriptions style={{marginTop: '20px'}} bordered={true} column={2}>
-        <Descriptions.Item label="模型名称">{evaluationDetail?.modelName}</Descriptions.Item>
-        {/* <Descriptions.Item label="评估状态">{getEvaluationStatus(evaluationDetail.status)}</Descriptions.Item> */}
-        <Descriptions.Item label="评估状态">{evaluationDetail?.status}</Descriptions.Item>
-        <Descriptions.Item label="引擎类型">{evaluationDetail?.engineType}</Descriptions.Item>
-        <Descriptions.Item label="测试数据集">{evaluationDetail?.datasetName}</Descriptions.Item>
-        {/* <Descriptions.Item label="创建时间">{moment(evaluationDetail.createAt).format('YYYY-MM-DD HH:mm')}</Descriptions.Item> */}
-        <Descriptions.Item label="创建时间">{evaluationDetail?.createdAt}</Descriptions.Item>
-        <Descriptions.Item label="设备类型">{evaluationDetail?.deviceType}</Descriptions.Item>
-        <Descriptions.Item label="设备数量">{evaluationDetail?.deviceNum}</Descriptions.Item>
+        <Descriptions.Item label="模型名称">{evaluationJob?.modelName}</Descriptions.Item>
+        <Descriptions.Item label="评估状态">{evaluationJob ? getJobStatus(evaluationJob.status) : ''}</Descriptions.Item>
+        {/* <Descriptions.Item label="评估状态">{evaluationJob?.status}</Descriptions.Item> */}
+        <Descriptions.Item label="引擎类型">{evaluationJob?.engine}</Descriptions.Item>
+        <Descriptions.Item label="测试数据集">{evaluationJob?.datasetPath}</Descriptions.Item>
+        <Descriptions.Item label="创建时间">{(evaluationJob && evaluationJob.createTime) ? moment(evaluationJob.createTime).format('YYYY-MM-DD HH:mm:ss') : ''}</Descriptions.Item>
+        {/* <Descriptions.Item label="创建时间">{evaluationJob?.createTime}</Descriptions.Item> */}
+        <Descriptions.Item label="设备类型">{evaluationJob?.deviceType}</Descriptions.Item>
+        <Descriptions.Item label="设备数量">{evaluationJob?.deviceNum}</Descriptions.Item>
       </Descriptions>
       <div className="ant-descriptions-title" style={{marginTop: '30px'}}>评估结果</div>
       <Button onClick={getLateastLogs}>点击获取评估结果</Button>
