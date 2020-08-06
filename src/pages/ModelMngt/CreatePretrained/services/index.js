e45ca91da389320d990c9df53eba1f6fd6d1d355
kgloveyou <hanjiangfeng2007@sina.com>
2020-08-06 11:47:24 +0800

    添加录入预置模型功能

--- src/pages/ModelMngt/CreatePretrained/services/index.js
+++ src/pages/ModelMngt/CreatePretrained/services/index.js
@@ -1,7 +1,19 @@
 import request from '@/utils/request';
+import { modelEvaluationType } from '@/utils/const';
 
 export async function getTrainingJobs(params) {
   return request(`/trainings`, {
     params
   });
+}
+
+export async function fetchPresetTemplates() {
+  return await request(`/templates`, {
+    params: {
+      pageNum: 1,
+      pageSize: 10000,
+      jobType: modelEvaluationType,
+      scope: 3,
+    }
+  })
 }
