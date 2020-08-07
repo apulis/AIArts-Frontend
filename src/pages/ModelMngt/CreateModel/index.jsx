4cc29e422dc761eeb17ddd4aa7eaacc3746bf30a
kgloveyou <hanjiangfeng2007@sina.com>
2020-08-07 11:31:18 +0800

    开启表单历史内容自动保存

--- src/pages/ModelMngt/CreateModel/index.jsx
+++ src/pages/ModelMngt/CreateModel/index.jsx
@@ -145,7 +145,7 @@ const CreateModel = props => {
           <Form
             form={form}
             onFinish={onFinish}
-            autoComplete="off"
+            // autoComplete="off"
             initialValues={{ modelFileType: modelFileType }}
           >
             <Form.Item
