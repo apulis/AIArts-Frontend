daa8ed61e845f390ce10daf81925f56cb196a34e
Ti-tanium <1158976264@qq.com>
2020-08-31 12:12:20 +0800

    add: wifi test host

--- config/proxy.js
+++ config/proxy.js
@@ -14,6 +14,7 @@ export default {
       // target: 'http://10.31.3.220:8080/',  //xu.ky
       // target: 'http://121.46.18.83:50088/',
       // target: 'http://10.31.3.231:9000/',
+      // target: 'http://10.31.3.100:9000/', // WIFI-Test
       changeOrigin: true,
       pathRewrite: {
         '^': '',
