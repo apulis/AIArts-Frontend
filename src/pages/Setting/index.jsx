045c57ad10377da6662ec29a7659b9e915c189f7
Han <xjhan1995@gmail.com>
2020-09-02 18:53:08 +0800

    Update settting

---
+++ src/pages/Setting/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Select, Button, Form } from 'antd';
+import { connect } from 'dva';
+
+const { Option } = Select;
+
+const FormItem = Form.Item;
+
+const Setting = ({ common, dispatch }) => {
+  const form = Form.useForm();
+  const { validateFields } = form;
+  const commonLayout = {
+    labelCol: { span: 4 },
+    wrapperCol: { span: 8 }
+  };
+  const changeInterval = async () => {
+    const result = await validateFields(['interval'])
+    console.log('result', result)
+  }
+  return (
+    <PageHeaderWrapper>
+      <div>
+        <h2>数据刷新时间配置</h2>
+        <Form
+          form={form}
+          {...commonLayout}
+        >
+          <FormItem name="interval">
+            <Select style={{ width: 120 }} defaultValue={common.interval}>
+              {[0, 3, 5, 10, 30].map(val => (
+                <Option value={val * 1000}>{val}秒</Option>
+              ))}
+            </Select>
+          </FormItem>
+        </Form>
+        
+        
+        <Button type="primary" onChange={changeInterval} style={{ marginLeft: '40px' }}>确定</Button>
+        </div>
+
+    </PageHeaderWrapper>
+  )
+}
+
+
+
+export default connect(({ common }) => ({ common }))(Setting);
