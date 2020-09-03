eb84e7dbef89319a7c678057f987b9747ae311b2
Han <xjhan1995@gmail.com>
2020-09-03 15:20:58 +0800

    完善GPU和NPU部分allocated num

--- src/models/resource.js
+++ src/models/resource.js
@@ -2,12 +2,34 @@ import { fetchCommonResource } from "@/services/common";
 
 let devices = {}
 
-export function beforeSumbitJob(isDistributed, deviceType, deviceNum) {
-  console.log('111', isDistributed, deviceType, deviceNum)
-  console.log('devices', devices)
-  return false
-  
+export function beforeSumbitJob(isDistributed, deviceType, deviceNum, distributedJobOptions) {
+  const detail = devices[deviceType].detail;
+  const allocatables = detail.map(val => val.allocatable);
+  const maxAllocatables = Math.max(allocatables);
+  maxAllocatables.sort((x, y) => y - x);
+  if (isDistributed) {
+    const { nodeNum } = distributedJobOptions;
+    if (deviceNum <= maxAllocatables[nodeNum - 1]) {
+      return true;
+    }
+  } else {
+    if (deviceNum <= maxAllocatables) {
+      return true;
+    }
+  }
+  return false;
 }
+
+const EnumDeviceTypes = {
+  'nvidia.com/gpu': 'GPU',
+  'huawei.com/npu': 'NPU',
+}
+
+export function checkIfGpuOrNpu(deviceType) {
+  const deviceStr = devices[deviceType].deviceStr;
+  return EnumDeviceTypes[deviceStr];
+}
+
 
 const ResourceModole = {
   namespace: 'resource',
@@ -21,7 +43,7 @@ const ResourceModole = {
         const { data: { resources } } = res;
         yield put({
           type: 'updateState',
-          payload: { devices: resources }
+          payload: { devices: resources },
         });
         devices = resources;
       }
