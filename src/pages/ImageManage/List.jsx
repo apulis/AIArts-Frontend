cd09fbb34900b28373e1fe724fbeb190d292cd7c
Han <xjhan1995@gmail.com>
2020-09-18 17:19:33 +0800

    增加镜像管理页面

--- src/pages/ImageManage/List.jsx
+++ src/pages/ImageManage/List.jsx
@@ -1,34 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import { Table } from 'antd';
+import ImageTable from './components/ImageTable';
 
 
 
 
 const List = () => {
 
-  const columns = [
-    {
-      title: 'ID',
-      dataIndex: 'imageId',
-    },
-    {
-      title: '名称',
-      dataIndex: 'name',
-    },
-    {
-      title: '创建时间',
-      render(_text, item) {
-        return <div>{moment(item.createdAt).format('YYYY-MM-DD HH:mm')}</div>
-      }
-    },
-  ]
+ 
+  
   
   return (
     <PageHeaderWrapper>
-      <Table
-        columns={columns}
-      />
+      <ImageTable />
     </PageHeaderWrapper>
   )
 }
---
+++ src/services/images.js
@@ -0,0 +1,18 @@
+import request from '../utils/request'
+
+
+export const deleteImages = async (id) => {
+  return await request('/saved_imgs/' + id, {
+    method: 'DELETE',
+  });
+}
+
+export const getImages = async (params) => {
+  return await request('/saved_imgs', {
+    params: {
+      pageNum: params.pageNum,
+      pageSize: params.pageSize,
+      searchWord: params.search,
+    }
+  });
+}
