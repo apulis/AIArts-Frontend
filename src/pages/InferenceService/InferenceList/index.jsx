d071e332995c3f20f7c3bab657d1be295c2f2a90
Han <xjhan1995@gmail.com>
2020-09-24 11:19:50 +0800

    修改运行时长显示问题

--- src/pages/InferenceService/InferenceList/index.jsx
+++ src/pages/InferenceService/InferenceList/index.jsx
@@ -117,7 +117,8 @@ const InferenceList = (props) => {
     },
     {
       title: '运行时长',
-      render: (text, item) => formatDuration(moment.duration(item.duration)),
+      align: 'center',
+      render: (text, item) => item.duration ? formatDuration(moment.duration(item.duration)) : '-',
     },
     {
       title: '服务地址',
