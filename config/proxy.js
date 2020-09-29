00b5c07d7aba191ef38859bbbcf5a7e446715d49
JiangWei <Wei.Jiang@apulis.com>
2020-09-29 12:30:57 +0800

    grafana汉化地址 & 代码模块英化demo

--- config/proxy.js
+++ config/proxy.js
@@ -10,7 +10,7 @@ export default {
     '/ai_arts/api/': {
       // target: 'http://10.31.3.120:9000/',
       // target: 'http://sandbox2-master.sigsus.cn:56511/',
-      target: 'http://10.31.3.66/',
+      target: 'http://219.133.167.42:30000/',
       // target: 'http://10.31.3.220:9999/',  //xu.ky
       // target: 'http://121.46.18.83:50088/',
       // target: 'http://10.31.3.231:9000/',
@@ -28,7 +28,7 @@ export default {
         '^': '',
       },
     },
-    '/endpoints/grafana/api/datasources/proxy/1/api/v1': {
+    '/endpoints_zh/api/datasources/proxy/1/api/v1': {
       target: 'https://atlas02.sigsus.cn/',
       changeOrigin: true,
       pathRewrite: {
