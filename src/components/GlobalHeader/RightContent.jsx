57b212dc429238da1c8e868224ce4808ad75d502
Han <xjhan1995@gmail.com>
2020-10-12 15:29:42 +0800

    更改选择语言按钮为官方提供

--- src/components/GlobalHeader/RightContent.jsx
+++ src/components/GlobalHeader/RightContent.jsx
@@ -15,35 +15,12 @@ const ENVTagColor = {
 
 const GlobalHeaderRight = (props) => {
   const intl = useIntl();
-  const [curLanguage, setCurLanguage] = useState('zh-CN');
   const { theme, layout } = props;
   let className = styles.right;
-
-  useEffect(() => {
-    const lang = getLocale();
-    setCurLanguage(lang);
-  }, []);
 
   if (theme === 'dark' && layout === 'topmenu') {
     className = `${styles.right}  ${styles.dark}`;
   }
-  const getLanguageBtnText = () => {
-    if (curLanguage === 'en-US') {
-      return '中文';
-    } else if (curLanguage === 'zh-CN') {
-      return 'English';
-    }
-  };
-  const switchLanguage = () => {
-    const lang = getLocale();
-    if (lang === 'en-US') {
-      setLocale('zh-CN');
-      setCurLanguage('zh-CN');
-    } else if (lang === 'zh-CN') {
-      setLocale('en-US');
-      setCurLanguage('en-US');
-    }
-  };
 
   return (
     <div
@@ -55,9 +32,6 @@ const GlobalHeaderRight = (props) => {
           当前虚拟集群 <DownOutlined />
         </a>
       </Dropdown> */}
-      <Button style={{ marginLeft: '20px' }} onClick={() => switchLanguage()}>
-        {getLanguageBtnText()}
-      </Button>
       <Button href="/custom-user-dashboard" style={{ marginLeft: '20px' }}>
         {intl.formatMessage({
           id: 'component.globalHeader.rightContent.globalHeaderRight.userManagementSystem',
@@ -69,7 +43,7 @@ const GlobalHeaderRight = (props) => {
           <Tag color={ENVTagColor[REACT_APP_ENV]}>{REACT_APP_ENV}</Tag>
         </span>
       )}
-      {/* <SelectLang className={styles.action} /> */}
+      <SelectLang className={styles.action} />
     </div>
   );
 };
