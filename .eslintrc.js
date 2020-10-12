5eac9efbb9a72c3ae213fd677603e7cbf7e96630
Han <xjhan1995@gmail.com>
2020-10-12 14:29:08 +0800

    添加自动 prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  extends: [require.resolve('@umijs/fabric/dist/eslint'), 'standard'],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
@@ -8,12 +8,8 @@ module.exports = {
   rules: {
     "no-console": "off",
     "no-shadow": "off",
-    "prefer-destructuring": "off"
-  },
-  rules: {
-    "no-console": "off",
-    "no-shadow": "off",
     "prefer-destructuring": "off",
-    "no-return-await": "off"
-  }
+    "no-return-await": "off",
+    "space-before-blocks": "on",
+  },
 };
