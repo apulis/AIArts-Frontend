18ca6a7f99ae5bc4eb73565246791e5e0a127857
Han <xjhan1995@gmail.com>
2020-10-14 18:27:31 +0800

    prettier code

--- src/locales/en-US/pages/ModelTraining/List.js
+++ src/locales/en-US/pages/ModelTraining/List.js
@@ -3,4 +3,4 @@ export default {
   'modelTraining.list.message.handle.success': 'Successfully operated',
   'modelTraining.list.modal.title.current.task.dont.stopped': 'The current job has not stopped',
   'modelTraining.list.modal.content.please.stop.task': 'Please stop this task first',
-}
+};
--- src/locales/zh-CN.js
+++ src/locales/zh-CN.js
@@ -48,5 +48,5 @@ export default {
   ...modelMngt,
   ...table,
   ...form,
-  ...ModelList
+  ...ModelList,
 };
