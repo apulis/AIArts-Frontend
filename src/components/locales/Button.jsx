e8774ae09bc8c144017f8fa38c974d4199b96947
Han <xjhan1995@gmail.com>
2020-11-02 10:11:58 +0800

    prettier code

--- src/components/locales/Button.jsx
+++ src/components/locales/Button.jsx
@@ -3,10 +3,7 @@ import { Button } from 'antd';
 import capFirstLetter from '../../src/utils/capFirstLetter';
 
 const UpperCaseButton = (prop) => {
-  return (
-    <Button {...props}>{capFirstLetter(props.children)}</Button>
-  )
+  return <Button {...props}>{capFirstLetter(props.children)}</Button>;
 };
 
-
-export default UpperCaseButton; 
+export default UpperCaseButton;
