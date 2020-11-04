51672bc4db07c03d76bdb57256f486ee14804aad
Han <xjhan1995@gmail.com>
2020-11-04 20:00:07 +0800

    增加 eslint 忽略规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,5 +39,6 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 'off',
     'implicit-arrow-linebreak': 'off',
     'no-param-reassign': 'off',
+    'no-return-await': 'off',
   },
 };
