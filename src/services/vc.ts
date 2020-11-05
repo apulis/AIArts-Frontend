5f41ad1c8856a362750de4a5ec644218afd48e14
Han <xjhan1995@gmail.com>
2020-11-05 15:18:54 +0800

    增加 VC 相关 service

---
+++ src/services/vc.ts
@@ -0,0 +1,23 @@
+import request from '@/utils/request';
+
+
+export const createVC = () => {
+
+}
+
+
+export const deleteVC = (vcName: string) => {
+
+}
+
+export const modifyVC = () => {
+  //
+}
+
+export const checkActiveJob = (vcName: string) => {
+
+}
+
+export const fetchVCList = (pageSize: number, pageNum: number, search: string) => {
+  //
+}
