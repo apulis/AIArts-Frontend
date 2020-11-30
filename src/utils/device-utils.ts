972aff10e1a90a6630703a56ca6d61cfa97e7730
Han <xjhan1995@gmail.com>
2020-11-30 15:12:52 +0800

    NPU 数量增加8的限制

--- src/utils/device-utils.ts
+++ src/utils/device-utils.ts
@@ -7,9 +7,11 @@ export const getAvailRegularDeviceNumber = (deviceName: string, deviceQuota: num
       arr.push(i);
     }
     return arr;
-  } else if (checkIfGpuOrNpu(deviceName) === 'NPU') {
+  }
+  if (checkIfGpuOrNpu(deviceName) === 'NPU') {
     const arr = [0] 
     for (let i = 1; i <= deviceQuota; i *= 2) {
+      if (i > 8) break;
       arr.push(i);
     }
     return arr;
@@ -25,12 +27,13 @@ export const getAvailPSDDeviceNumber = (deviceName: string, deviceQuota: number,
       arr.push(i);
     }
     return arr;
-  } else if (checkIfGpuOrNpu(deviceName) === 'NPU') {
+  }
+  if (checkIfGpuOrNpu(deviceName) === 'NPU') {
     if (deviceQuota >= 8) {
       return [8];
-    } else {
-      return [];
-    }
+    } 
+    return [];
+    
   }
   return [];
 }; 
