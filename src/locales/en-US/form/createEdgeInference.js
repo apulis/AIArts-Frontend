e2f25b6f2ac13c93fc31b64a63d716cba45b1845
Han <xjhan1995@gmail.com>
2020-12-08 11:52:00 +0800

    修改多语言错误

--- src/locales/en-US/form/createEdgeInference.js
+++ src/locales/en-US/form/createEdgeInference.js
@@ -1,18 +1,18 @@
 export default {
-  'edgeInferenceJobCreate.label.jobName': 'FD Server URL',
+  'edgeInferenceJobCreate.label.jobName': 'Inference Name',
   'edgeInferenceJobCreate.label.conversionType': 'Type',
   'edgeInferenceJobCreate.label.inputPath': 'Input Path',
   'edgeInferenceJobCreate.label.outputPath': 'Output Path',
   'edgeInferenceJobCreate.label.transformParams': 'Transform Params',
 
-  'edgeInferenceJobCreate.placeholder.inputInferenceName': 'Please Input FD Server URL',
+  'edgeInferenceJobCreate.placeholder.inputInferenceName': 'Please Input Inference Name',
   'edgeInferenceJobCreate.placeholder.selectType': 'Please Select Type',
   'edgeInferenceJobCreate.placeholder.inputInputPath': 'Please Input Input Path',
   'edgeInferenceJobCreate.placeholder.inputOutputPath': 'Please Input Output Path',
   'edgeInferenceJobCreate.placeholder.selectParamsType': 'Please Select Params Type',
   'edgeInferenceJobCreate.placeholder.inputParamsValue': 'Please Input Params Value',
 
-  'edgeInferenceJobCreate.rule.needName': 'Please Input FD Server URL！',
+  'edgeInferenceJobCreate.rule.needName': 'Please Input Inference Name！',
   'edgeInferenceJobCreate.rule.needType': 'Please Select Type!',
   'edgeInferenceJobCreate.rule.needInputPath': 'Please Input Input Path！',
   'edgeInferenceJobCreate.rule.needOutputPath': 'Please Input Output Path！',
--- src/locales/zh-CN/form/createEdgeInference.js
+++ src/locales/zh-CN/form/createEdgeInference.js
@@ -1,18 +1,18 @@
 export default {
-  'edgeInferenceJobCreate.label.jobName': '推理地址',
+  'edgeInferenceJobCreate.label.jobName': '推理名称',
   'edgeInferenceJobCreate.label.conversionType': '类型',
   'edgeInferenceJobCreate.label.inputPath': '输入路径',
   'edgeInferenceJobCreate.label.outputPath': '输出路径',
   'edgeInferenceJobCreate.label.transformParams': '转换参数',
 
-  'edgeInferenceJobCreate.placeholder.inputInferenceName': '请输入推理服务器地址',
+  'edgeInferenceJobCreate.placeholder.inputInferenceName': '请输入推理名称',
   'edgeInferenceJobCreate.placeholder.selectType': '请选择类型',
   'edgeInferenceJobCreate.placeholder.inputInputPath': '请填写输入路径',
   'edgeInferenceJobCreate.placeholder.inputOutputPath': '请填写输出路径',
   'edgeInferenceJobCreate.placeholder.selectParamsType': '请选择参数类型',
   'edgeInferenceJobCreate.placeholder.inputParamsValue': '请填写参数值',
 
-  'edgeInferenceJobCreate.rule.needName': '请输入推理服务器地址！',
+  'edgeInferenceJobCreate.rule.needName': '请输入推理名称！',
   'edgeInferenceJobCreate.rule.needType': '请选择类型！',
   'edgeInferenceJobCreate.rule.needInputPath': '请填写输入路径！',
   'edgeInferenceJobCreate.rule.needOutputPath': '请填写输出路径！',
