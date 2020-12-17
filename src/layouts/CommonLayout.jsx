ddadb5e34397d06b3831efcf4c7499d0319998f5
Han <xjhan1995@gmail.com>
2020-12-17 16:45:09 +0800

    轮询获取虚拟集群

--- src/layouts/CommonLayout.jsx
+++ src/layouts/CommonLayout.jsx
@@ -3,8 +3,9 @@ import { connect } from 'dva';
 import { PageLoading } from '@ant-design/pro-layout';
 import { Result } from 'antd';
 import { formatMessage } from 'umi';
+import useInterval from '@/hooks/useInterval';
 
-const CommonLayout = ({ children, dispatch, resource, user }) => {
+const CommonLayout = ({ children, dispatch, resource, user, common }) => {
   useEffect(() => {
     dispatch({
       type: 'resource/fetchResource',
@@ -12,6 +13,26 @@ const CommonLayout = ({ children, dispatch, resource, user }) => {
   }, []);
   const { currentUser } = user;
   const { currentVC, userName } = currentUser;
+  
+  useInterval(() => {
+    if (currentVC?.length > 0) {
+      if (!localStorage.vc || !currentVC.includes(localStorage.vc)) {
+        dispatch({
+          type: 'vc/userSelectVC',
+          payload: {
+            vcName: currentVC[0],
+          },
+        });
+      } else if (localStorage.vc) {
+        dispatch({
+          type: 'vc/userSelectVC',
+          payload: {
+            vcName: localStorage.vc,
+          },
+        });
+      }
+    }
+  }, common.interval)
   if (currentVC?.length === 0 && userName) {
     return (
       <Result
@@ -21,27 +42,11 @@ const CommonLayout = ({ children, dispatch, resource, user }) => {
       />
     );
   }
-  if (currentVC?.length > 0) {
-    if (!localStorage.vc || !currentVC.includes(localStorage.vc)) {
-      dispatch({
-        type: 'vc/userSelectVC',
-        payload: {
-          vcName: currentVC[0],
-        },
-      });
-    } else if (localStorage.vc) {
-      dispatch({
-        type: 'vc/userSelectVC',
-        payload: {
-          vcName: localStorage.vc,
-        },
-      });
-    }
-  }
+  
   if (Object.keys(resource.devices).length === 0) {
     return <PageLoading />;
   }
   return <>{children}</>;
 };
 
-export default connect(({ resource, user }) => ({ resource, user }))(CommonLayout);
+export default connect(({ resource, user, common }) => ({ resource, user, common }))(CommonLayout);
