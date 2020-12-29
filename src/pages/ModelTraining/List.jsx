c86b6e61ee016500f9d43d8ea0e79d4b20b45faf
Han <xjhan1995@gmail.com>
2020-12-29 14:23:11 +0800

    修改模型训练表格列宽度

--- src/pages/ModelTraining/List.jsx
+++ src/pages/ModelTraining/List.jsx
@@ -245,7 +245,6 @@ const List = (props) => {
       title: formatMessage({ id: 'ManagePrivilegeJob.table.title.is.privilege' }),
       dataIndex: 'isPrivileged',
       align: 'center',
-      width: '6%',
       render(isPrivileged) {
         return isPrivileged ? formatMessage({ id: 'ManagePrivilegeJob.table.title.is' }) : formatMessage({ id: 'ManagePrivilegeJob.table.title.not' })
       }
