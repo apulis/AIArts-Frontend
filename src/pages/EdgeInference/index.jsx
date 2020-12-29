27ffd51298746f3773dcd8610ed265afeb6f81cd
Han <xjhan1995@gmail.com>
2020-12-29 17:08:05 +0800

    修改边缘推理部分eslint报错

--- src/pages/EdgeInference/index.jsx
+++ src/pages/EdgeInference/index.jsx
@@ -1,29 +1,28 @@
 import { message, Table, Modal, Form, Input, Button, Select, Card } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import React, { useState, useEffect } from 'react';
-import { Link } from 'umi';
-import { getEdgeInferences, submit, getFD, submitFD, push, deleteEG } from './service';
-import styles from './index.less';
 import moment from 'moment';
 import { PAGEPARAMS, sortText } from '@/utils/const';
 import { SyncOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import _ from 'lodash';
 import useInterval from '@/hooks/useInterval';
-import { connect } from 'dva';
-import { useIntl, formatMessage } from 'umi';
+import { useIntl, Link, connect } from 'umi';
+import styles from './index.less';
+import { getEdgeInferences, getFD, submitFD, push, deleteEG } from './service';
 
 const { Option } = Select;
 const { Search } = Input;
 const { confirm } = Modal;
-const typeText = {
-  converting: formatMessage({ id: 'edgeInference.list.typeText.converting' }),
-  pushing: formatMessage({ id: 'edgeInference.list.typeText.push' }),
-  'push success': formatMessage({ id: 'edgeInference.list.typeText.push.success' }),
-  'push failed': formatMessage({ id: 'edgeInference.list.typeText.push.failed' }),
-};
+
 
 const EdgeInference = (props) => {
-  const intl = useIntl();
+  const { formatMessage } = useIntl();
+  const typeText = {
+    converting: formatMessage({ id: 'edgeInference.list.typeText.converting' }),
+    pushing: formatMessage({ id: 'edgeInference.list.typeText.push' }),
+    'push success': formatMessage({ id: 'edgeInference.list.typeText.push.success' }),
+    'push failed': formatMessage({ id: 'edgeInference.list.typeText.push.failed' }),
+  };
   const [form] = Form.useForm();
   const [jobs, setJobs] = useState([]);
   const [fdInfo, setFdInfo] = useState({ username: '', url: '', password: '' });
@@ -40,21 +39,6 @@ const EdgeInference = (props) => {
     order: '',
   });
   const { currentSelectedVC, jobMaxTimeSecond } = props.vc;
-
-  useEffect(() => {
-    getData();
-    return () => {
-      getEdgeInferences.cancel && getEdgeInferences.cancel();
-    };
-  }, [pageParams, sortedInfo]);
-
-  useInterval(() => {
-    getData(null, true);
-  }, props.common.interval);
-
-  useEffect(() => {
-    getFdInfo();
-  }, []);
 
   const getData = async (text, isInterval) => {
     !isInterval && setLoading(true);
@@ -80,107 +64,7 @@ const EdgeInference = (props) => {
   const pageParamsChange = (page, count) => {
     setPageParams({ pageNum: page, pageSize: count });
   };
-
-  const onDelete = (id) => {
-    confirm({
-      title: intl.formatMessage({ id: 'edgeInference.list.onDelete.title' }),
-      icon: <ExclamationCircleOutlined />,
-      okText: intl.formatMessage({ id: 'edgeInference.list.onDelete.okText' }),
-      okType: 'danger',
-      cancelText: intl.formatMessage({ id: 'edgeInference.list.onDelete.cancelText' }),
-      onOk: async () => {
-        const { code } = await deleteEG(id);
-        if (code === 0) {
-          // 若删除的是当前页最后一项，且页数不是第一页，则将页数减一
-          if (jobs.length == 1 && pageParams.pageNum > 1) {
-            setPageParams({ ...pageParams, pageNum: pageParams.pageNum - 1 });
-          } else {
-            getData();
-          }
-          message.success(intl.formatMessage({ id: 'edgeInference.list.onDelete.ok.tips' }));
-        }
-      },
-      onCancel() {},
-    });
-  };
-
-  const columns = [
-    {
-      title: intl.formatMessage({ id: 'edgeInferenceList.table.column.id' }),
-      dataIndex: 'jobId',
-      render: (id) => <span style={{ fontFamily: 'Consolas' }}>{id}</span>,
-    },
-    {
-      title: intl.formatMessage({ id: 'edgeInferenceList.table.column.name' }),
-      dataIndex: 'jobName',
-      key: 'jobName',
-      sorter: true,
-      sortOrder: sortedInfo.columnKey === 'jobName' && sortedInfo.order,
-    },
-    {
-      title: intl.formatMessage({ id: 'job.rest.time' }),
-      render: (text, item) => {
-        const status = item.status || item.jobStatus;
-        const lastedTime = item.duration;
-        if (status === 'running') {
-          if (!jobMaxTimeSecond) {
-            return '-';
-          }
-          const restTime = Math.floor((jobMaxTimeSecond - lastedTime) / 60);
-          return restTime + intl.formatMessage({ id: 'job.rest.minute' });
-        }
-        return '-';
-      },
-      ellipsis: true,
-      width: '8%',
-    },
-    {
-      title: intl.formatMessage({ id: 'edgeInferenceList.table.column.type' }),
-      dataIndex: 'modelconversionType',
-    },
-    {
-      title: intl.formatMessage({ id: 'edgeInferenceList.table.column.time' }),
-      dataIndex: 'jobTime',
-      key: 'jobTime',
-      sorter: true,
-      render: (text) => moment(text).format('YYYY-MM-DD HH:mm:ss'),
-      sortOrder: sortedInfo.columnKey === 'jobTime' && sortedInfo.order,
-    },
-    {
-      title: intl.formatMessage({ id: 'edgeInferenceList.table.column.status' }),
-      render: (item) => {
-        const { jobStatus, modelconversionStatus } = item;
-        let status = typeText[modelconversionStatus];
-        if (modelconversionStatus === 'converting')
-          status =
-            jobStatus === 'finished'
-              ? intl.formatMessage({ id: 'edgeInference.list.converting.result.success' })
-              : jobStatus === 'failed' || jobStatus === 'error'
-              ? intl.formatMessage({ id: 'edgeInference.list.converting.result.error' })
-              : status;
-        return <span>{status}</span>;
-      },
-    },
-    {
-      title: intl.formatMessage({ id: 'edgeInferenceList.table.column.action' }),
-      render: (item) => {
-        const { jobStatus, modelconversionStatus, jobId } = item;
-        const disabled =
-          !(modelconversionStatus === 'converting' && jobStatus === 'finished') || pushId === jobId;
-        return (
-          <>
-            <a onClick={() => onPush(jobId)} disabled={disabled}>
-              {intl.formatMessage({ id: 'edgeInferenceList.table.column.action.push' })}
-            </a>
-            <a style={{ color: 'red', marginLeft: 16 }} onClick={() => onDelete(jobId)}>
-              {intl.formatMessage({ id: 'edgeInferenceList.table.column.action.delete' })}
-            </a>
-          </>
-        );
-      },
-    },
-  ];
-
+  
   const getFdInfo = async () => {
     let info = {};
     const { code, data } = await getFD();
@@ -198,15 +82,116 @@ const EdgeInference = (props) => {
       const { code, data } = await push({ jobId: id });
       if (code === 0) {
         getData();
-        message.success(intl.formatMessage({ id: 'edgeInference.list.onPush.success' }));
+        message.success(formatMessage({ id: 'edgeInference.list.onPush.success' }));
       } else {
         setPushId('');
       }
     } else {
-      message.warning(intl.formatMessage({ id: 'edgeInference.list.onPush.error' }));
+      message.warning(formatMessage({ id: 'edgeInference.list.onPush.error' }));
       setModalFlag2(true);
     }
   };
+
+  const onDelete = (id) => {
+    confirm({
+      title: formatMessage({ id: 'edgeInference.list.onDelete.title' }),
+      icon: <ExclamationCircleOutlined />,
+      okText: formatMessage({ id: 'edgeInference.list.onDelete.okText' }),
+      okType: 'danger',
+      cancelText: formatMessage({ id: 'edgeInference.list.onDelete.cancelText' }),
+      onOk: async () => {
+        const { code } = await deleteEG(id);
+        if (code === 0) {
+          // 若删除的是当前页最后一项，且页数不是第一页，则将页数减一
+          if (jobs.length === 1 && pageParams.pageNum > 1) {
+            setPageParams({ ...pageParams, pageNum: pageParams.pageNum - 1 });
+          } else {
+            getData();
+          }
+          message.success(formatMessage({ id: 'edgeInference.list.onDelete.ok.tips' }));
+        }
+      },
+      onCancel() {},
+    });
+  };
+
+  const columns = [
+    {
+      title: formatMessage({ id: 'edgeInferenceList.table.column.id' }),
+      dataIndex: 'jobId',
+      render: (id) => <span style={{ fontFamily: 'Consolas' }}>{id}</span>,
+    },
+    {
+      title: formatMessage({ id: 'edgeInferenceList.table.column.name' }),
+      dataIndex: 'jobName',
+      key: 'jobName',
+      sorter: true,
+      sortOrder: sortedInfo.columnKey === 'jobName' && sortedInfo.order,
+    },
+    {
+      title: formatMessage({ id: 'job.rest.time' }),
+      render: (text, item) => {
+        const status = item.status || item.jobStatus;
+        const lastedTime = item.duration;
+        if (status === 'running') {
+          if (!jobMaxTimeSecond) {
+            return '-';
+          }
+          const restTime = Math.floor((jobMaxTimeSecond - lastedTime) / 60);
+          return restTime + formatMessage({ id: 'job.rest.minute' });
+        }
+        return '-';
+      },
+      ellipsis: true,
+      width: '8%',
+    },
+    {
+      title: formatMessage({ id: 'edgeInferenceList.table.column.type' }),
+      dataIndex: 'modelconversionType',
+    },
+    {
+      title: formatMessage({ id: 'edgeInferenceList.table.column.time' }),
+      dataIndex: 'jobTime',
+      key: 'jobTime',
+      sorter: true,
+      render: (text) => moment(text).format('YYYY-MM-DD HH:mm:ss'),
+      sortOrder: sortedInfo.columnKey === 'jobTime' && sortedInfo.order,
+    },
+    {
+      title: formatMessage({ id: 'edgeInferenceList.table.column.status' }),
+      render: (item) => {
+        const { jobStatus, modelconversionStatus } = item;
+        let status = typeText[modelconversionStatus];
+        if (modelconversionStatus === 'converting')
+          status =
+            jobStatus === 'finished'
+              ? formatMessage({ id: 'edgeInference.list.converting.result.success' })
+              : jobStatus === 'failed' || jobStatus === 'error'
+              ? formatMessage({ id: 'edgeInference.list.converting.result.error' })
+              : status;
+        return <span>{status}</span>;
+      },
+    },
+    {
+      title: formatMessage({ id: 'edgeInferenceList.table.column.action' }),
+      render: (item) => {
+        const { jobStatus, modelconversionStatus, jobId } = item;
+        const disabled =
+          !(modelconversionStatus === 'converting' && jobStatus === 'finished') || pushId === jobId;
+        return (
+          <>
+            <a onClick={() => onPush(jobId)} disabled={disabled}>
+              {formatMessage({ id: 'edgeInferenceList.table.column.action.push' })}
+            </a>
+            <a style={{ color: 'red', marginLeft: 16 }} onClick={() => onDelete(jobId)}>
+              {formatMessage({ id: 'edgeInferenceList.table.column.action.delete' })}
+            </a>
+          </>
+        );
+      },
+    },
+  ];
+
 
   const openSettings = async () => {
     await getFdInfo();
@@ -218,7 +203,7 @@ const EdgeInference = (props) => {
     form.validateFields().then(async (values) => {
       const { code, data } = await submitFD(values);
       if (code === 0) {
-        message.success(intl.formatMessage({ id: 'edgeInference.list.onSubmitFD.success' }));
+        message.success(formatMessage({ id: 'edgeInference.list.onSubmitFD.success' }));
         getFdInfo();
         setModalFlag2(false);
       }
@@ -235,36 +220,52 @@ const EdgeInference = (props) => {
   const getOptions = () => {
     const statusMap = [
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.all' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.all' }),
         status: '',
       },
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.push.ing' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.push.ing' }),
         status: 'finished-pushing',
       },
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.push.success' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.push.success' }),
         status: 'finished-push success',
       },
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.push.error' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.push.error' }),
         status: 'finished-push failed',
       },
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.converting.ing' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.converting.ing' }),
         status: 'running,scheduling,queued,unapproved-converting',
       },
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.converting.success' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.converting.success' }),
         status: 'finished-converting',
       },
       {
-        text: intl.formatMessage({ id: 'edgeInference.list.statusMap.converting.error' }),
+        text: formatMessage({ id: 'edgeInference.list.statusMap.converting.error' }),
         status: 'error,failed-converting',
       },
     ];
     return statusMap.map((i) => <Option value={i.status}>{i.text}</Option>);
   };
+
+  
+  useEffect(() => {
+    getData();
+    return () => {
+      getEdgeInferences.cancel && getEdgeInferences.cancel();
+    };
+  }, [pageParams, sortedInfo]);
+
+  useInterval(() => {
+    getData(null, true);
+  }, props.common.interval);
+
+  useEffect(() => {
+    getFdInfo();
+  }, []);
 
   const onSearchChange = (v, type) => {
     type === 1 ? setStatusType(v) : setName(v);
@@ -277,15 +278,15 @@ const EdgeInference = (props) => {
         <div className={styles.edgeInferences}>
           <Link to="/Inference/EdgeInference/submit">
             <Button type="primary">
-              {intl.formatMessage({ id: 'edgeInference.list.add.inference' })}
+              {formatMessage({ id: 'edgeInference.list.add.inference' })}
             </Button>
           </Link>
           <Button type="primary" style={{ margin: '0 16px 16px' }} onClick={openSettings}>
-            {intl.formatMessage({ id: 'edgeInference.list.setting' })}
+            {formatMessage({ id: 'edgeInference.list.setting' })}
           </Button>
           {fdInfo.url && (
             <Button type="primary" onClick={() => window.open(fdInfo.url)}>
-              {intl.formatMessage({ id: 'edgeInference.list.fdServer' })}
+              {formatMessage({ id: 'edgeInference.list.fdServer' })}
             </Button>
           )}
           <div className={styles.searchWrap}>
@@ -293,14 +294,14 @@ const EdgeInference = (props) => {
               {getOptions()}
             </Select>
             <Search
-              placeholder={intl.formatMessage({ id: 'edgeInference.list.placeholder.search' })}
+              placeholder={formatMessage({ id: 'edgeInference.list.placeholder.search' })}
               enterButton
               onChange={(e) => setName(e.target.value)}
               onSearch={(v) => onSearchChange(v, 2)}
             />
             <Button
               onClick={() =>
-                getData(intl.formatMessage({ id: 'edgeInference.list.fresh.success' }))
+                getData(formatMessage({ id: 'edgeInference.list.fresh.success' }))
               }
               icon={<SyncOutlined />}
             />
@@ -314,9 +315,9 @@ const EdgeInference = (props) => {
               total: total,
               showQuickJumper: true,
               showTotal: (total) =>
-                `${intl.formatMessage({
+                `${formatMessage({
                   id: 'edgeInferenceList.table.pagination.showTotal.prefix',
-                })} ${total} ${intl.formatMessage({
+                })} ${total} ${formatMessage({
                   id: 'edgeInferenceList.table.pagination.showTotal.suffix',
                 })}`,
               showSizeChanger: true,
@@ -331,7 +332,7 @@ const EdgeInference = (props) => {
       </Card>
       {modalFlag2 && (
         <Modal
-          title={intl.formatMessage({ id: 'edgeInference.list.setting' })}
+          title={formatMessage({ id: 'edgeInference.list.setting' })}
           visible={modalFlag2}
           onCancel={() => setModalFlag2(false)}
           destroyOnClose
@@ -339,58 +340,58 @@ const EdgeInference = (props) => {
           className="settingModal"
           footer={[
             <Button onClick={() => setModalFlag2(false)}>
-              {intl.formatMessage({ id: 'edgeInference.list.cancel' })}
+              {formatMessage({ id: 'edgeInference.list.cancel' })}
             </Button>,
             <Button type="primary" loading={btnLoading} onClick={onSubmitFD}>
-              {intl.formatMessage({ id: 'edgeInference.list.save' })}
+              {formatMessage({ id: 'edgeInference.list.save' })}
             </Button>,
           ]}
         >
           <Form form={form} initialValues={fdInfo}>
             <Form.Item
-              label={intl.formatMessage({ id: 'confirmEdgeInferencePush.label.url' })}
+              label={formatMessage({ id: 'confirmEdgeInferencePush.label.url' })}
               name="url"
               rules={[
                 {
                   required: true,
-                  message: intl.formatMessage({ id: 'confirmEdgeInferencePush.rule.needUrl' }),
+                  message: formatMessage({ id: 'confirmEdgeInferencePush.rule.needUrl' }),
                 },
               ]}
             >
               <Input
-                placeholder={intl.formatMessage({
+                placeholder={formatMessage({
                   id: 'confirmEdgeInferencePush.placeholder.inputUrl',
                 })}
               />
             </Form.Item>
             <Form.Item
-              label={intl.formatMessage({ id: 'confirmEdgeInferencePush.label.username' })}
+              label={formatMessage({ id: 'confirmEdgeInferencePush.label.username' })}
               name="username"
               rules={[
                 {
                   required: true,
-                  message: intl.formatMessage({ id: 'confirmEdgeInferencePush.rule.needUsername' }),
+                  message: formatMessage({ id: 'confirmEdgeInferencePush.rule.needUsername' }),
                 },
               ]}
             >
               <Input
-                placeholder={intl.formatMessage({
+                placeholder={formatMessage({
                   id: 'confirmEdgeInferencePush.placeholder.inputUsername',
                 })}
               />
             </Form.Item>
             <Form.Item
-              label={intl.formatMessage({ id: 'confirmEdgeInferencePush.label.password' })}
+              label={formatMessage({ id: 'confirmEdgeInferencePush.label.password' })}
               name="password"
               rules={[
                 {
                   required: true,
-                  message: intl.formatMessage({ id: 'confirmEdgeInferencePush.rule.needPassword' }),
+                  message: formatMessage({ id: 'confirmEdgeInferencePush.rule.needPassword' }),
                 },
               ]}
             >
               <Input
-                placeholder={intl.formatMessage({
+                placeholder={formatMessage({
                   id: 'confirmEdgeInferencePush.placeholder.inputPassword',
                 })}
               />
